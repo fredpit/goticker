44943797001e760767f44de5acab3ff188f9c70f
fredpit <frederic.pitoizet@gmail.com>
2020-12-01 09:24:14 +0100

    All ptr

--- go-ticker.go
+++ go-ticker.go
@@ -16,20 +16,20 @@ type Ticker struct {
 }
 
 // New : create a ticker with no delay at start
-func New(d time.Duration) Ticker {
+func New(d time.Duration) *Ticker {
 	return NewWithInitialDelay(d, 0)
 }
 
 // NewWithInitialDelay : create a ticler with an initial delay
-func NewWithInitialDelay(d, dl time.Duration) Ticker {
-	return Ticker{
+func NewWithInitialDelay(d, dl time.Duration) *Ticker {
+	return &Ticker{
 		nextTick:     time.Now().Add(dl),
 		tickDuration: d,
 	}
 }
 
 // Tick : test if the ticker has ticked
-func (t Ticker) Tick() bool {
+func (t *Ticker) Tick() bool {
 	if time.Now().After(t.nextTick) {
 		t.Reset()
 		return true
@@ -38,6 +38,6 @@ func (t Ticker) Tick() bool {
 }
 
 // Reset : restart tick delay
-func (t Ticker) Reset() {
+func (t *Ticker) Reset() {
 	t.nextTick = time.Now().Add(t.tickDuration)
 }
