d7dab3371b3cb3a90e7deb68cd3bce7f47b006f7
fredpit <frederic.pitoizet@gmail.com>
2020-12-01 08:34:52 +0100

    Initial code commit

---
+++ go-ticker.go
@@ -0,0 +1,38 @@
+/*
+ * GO Ticker
+ *
+ * Frédéric PITOIZET.	2020-12-01.
+ *
+ */
+
+package goticker
+
+import "time"
+
+// Ticker : struct containing ticker
+type Ticker struct {
+	nextTick     time.Time
+	tickDuration time.Duration
+}
+
+// New : create a ticker with no delay at start
+func New(d time.Duration) Ticker {
+	return NewWithInitialDelay(d, 0)
+}
+
+// NewWithInitialDelay : create a ticler with an initial delay
+func NewWithInitialDelay(d, dl time.Duration) Ticker {
+	return Ticker{
+		nextTick:     time.Now().Add(d),
+		tickDuration: d,
+	}
+}
+
+// Tick : test if the ticker has ticked
+func (t Ticker) Tick() bool {
+	if time.Now().After(t.nextTick) {
+		t.nextTick = time.Now().Add(t.tickDuration)
+		return true
+	}
+	return false
+}
